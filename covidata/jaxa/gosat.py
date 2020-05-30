0a58f574d346e21faa88ce66f8b466693a908c98
joaogui <joaoguilhermearujo@gmail.com>
2020-05-30 17:41:07 -0300

    gosat example

---
+++ covidata/jaxa/gosat.py
@@ -0,0 +1,28 @@
+import os
+
+import requests
+import pandas as pd
+
+
+
+def get(path=''):
+    """Loads Data
+    # Arguments
+        path: path where to cache the dataset locally
+            (relative to ~/.keras/datasets).
+    # Returns
+        Dataframe or numpy arrays (if image)
+    """
+
+    if not os.path.exists('/tmp/gosat.tsv'):
+        with open('/tmp/gosat.tsv', 'w') as data:
+            r = requests.get('https://www.eorc.jaxa.jp/GOSAT/GPCG/download/data-g2-201902.txt')
+            lines = r.text.split('\n')[11:-1]
+            # data = open('/tmp/covidata/gosat.tsv', 'w')
+            for l in lines:
+                l = '\t'.join(l.split())
+                data.write(l)
+    
+    df = pd.read_csv(data)
+    
+    return df

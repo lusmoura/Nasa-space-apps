cdaed851000eeb0921b403d0bb12d17604a4f86d
joaogui <joaoguilhermearujo@gmail.com>
2020-05-31 11:38:13 -0300

    ver 0.0.4

---
+++ build/lib/covidata/nasa/earthdata_login.py
@@ -0,0 +1,11 @@
+headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36'}
+
+login_data = {
+        'utf8': '✓',
+        'client_id': 'YQOhivHfMTau88rjbMOVyg',
+        'redirect_uri': 'https://daac.ornl.gov/cgi-bin/urs/urs_logon_proc.pl',
+        'response_type': 'code',
+        'state': 'https://daac.ornl.gov/',
+        'stay_in': '1',
+        'commit': 'Log in',
+}
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="covidata",
-    version="0.0.3",
+    version="0.0.4",
     author="Control-space",
     author_email="author@example.com",
     description="Easily access NASA, ESA and JAXA data",

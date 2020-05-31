e15bdac744e618d32e553f1443739adf29ad8074
joaogui <joaoguilhermearujo@gmail.com>
2020-05-31 09:08:35 -0300

    function -> class for the rest

--- covidata/esa/opendata.py
+++ covidata/esa/opendata.py
@@ -4,34 +4,40 @@ import requests
 import pandas as pd
 
 
-
-def get(subset=None, verbose=False):
-    """Loads Data
-    # Arguments
-        subset: the last row to load in the Dataframe
-        verbose
-    # Returns
-        Dataframe with the data
-    """
-
-    if not os.path.exists('/tmp/opendata.csv'):
-        if verbose:
-            print("Cache not found, downloading data...")
-        with open(f'/tmp/opendata.csv', 'w') as data:
-            r = requests.get(f'https://opendata.ecdc.europa.eu/covid19/casedistribution/csv')
+class OpenData:
+    def __init__(self, subset=-1, verbose=False):
+        """Loads Data
+        # Arguments
+            subset: the last row to load in the Dataframe
+            verbose
+        # Returns
+            Dataframe with the data
+        """
+        super().__init__()
+        if not os.path.exists('/tmp/opendata.csv'):
             if verbose:
-                print("Downloaded data")
-            lines = r.text.split('\r\n')[:subset]
-            for l in lines:
-                l += '\n'
-                data.write(l)
-    
-    df = pd.read_csv('/tmp/opendata.csv')
-    if verbose:
-        print("Dataset loaded")
-    return df
+                print("Cache not found, downloading data...")
+            try:
+                with open(f'/tmp/opendata.csv', 'w') as data:
+                    r = requests.get(f'https://opendata.ecdc.europa.eu/covid19/casedistribution/csv')
+                    if verbose:
+                        print("Downloaded data")
+                    lines = r.text.split('\r\n')[:subset]
+                    for l in lines:
+                        l += '\n'
+                        data.write(l)
+            except:
+                os.remove('/tmp/opendata.csv')
+                raise ConnectionError("You need an internet connection to download the data")
+        
+        df = pd.read_csv('/tmp/opendata.csv')
+        if verbose:
+            print("Dataset loaded")
+        self.df = df
+    def summarize(self):
+        return self.df.describe()
 
 
 if __name__ == '__main__':
-    df = get(subset=100, verbose=True)
-    print(df.head())
+    OD = OpenData(subset=100, verbose=True)
+    print(OD.summarize())
--- covidata/nasa/landslide.py
+++ covidata/nasa/landslide.py
@@ -5,33 +5,41 @@ import pandas as pd
 
 
 
-def get(subset=-1, verbose=False):
-    """Loads Data
-    # Arguments
-        subset: the last row to load in the Dataframe
-        verbose
-    # Returns
-        Dataframe with the data
-    """
+class Landslide:
+    def __init__(self, subset=-1, verbose=False):
+        """Loads Data
+        # Arguments
+            subset: the last row to load in the Dataframe
+            verbose
+        # Returns
+            Dataframe with the data
+        """
 
-    if not os.path.exists('/tmp/landsliade_data.csv'):
-        if verbose:
-            print("Cache not found, downloading data...")
-        with open(f'/tmp/landsliade_data.csv', 'w') as data:
-            r = requests.get('https://data.nasa.gov/api/views/dd9e-wu2v/rows.csv?accessType=DOWNLOAD')
+        if not os.path.exists('/tmp/landsliade_data.csv'):
             if verbose:
-                print("Downloaded data")
-            lines = r.text.split('\n')[:subset]
-            for l in lines:
-                l += "\n"
-                data.write(l)
+                print("Cache not found, downloading data...")
+            try:
+                with open(f'/tmp/landsliade_data.csv', 'w') as data:
+                    r = requests.get('https://data.nasa.gov/api/views/dd9e-wu2v/rows.csv?accessType=DOWNLOAD')
+                    if verbose:
+                        print("Downloaded data")
+                    lines = r.text.split('\n')[:subset]
+                    for l in lines:
+                        l += "\n"
+                        data.write(l)
+            except:
+                os.remove('/tmp/landslide_data.csv')
+                raise ConnectionError("You need an internet connection to download the data")
+        
+        df = pd.read_csv('/tmp/landsliade_data.csv')
+        if verbose:
+            print("Dataset loaded")
+        self.df =  df
     
-    df = pd.read_csv('/tmp/landsliade_data.csv')
-    if verbose:
-        print("Dataset loaded")
-    return df
+    def summarize(self):
+        return self.df.describe()
 
 
 if __name__ == '__main__':
-    df = get(subset=100, verbose=True)
-    print(df.head())
+    LS = Landslide(subset=100, verbose=True)
+    print(LS.summarize())
